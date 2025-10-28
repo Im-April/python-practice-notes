8a4d5e92fa0be9ee8e55ca5cf74f3414c703d55c
April <ghwon4640@gmail.com>
2025-10-29 02:14:12 +0900

    연산자 복습

---
+++ 6시간파이썬/operator1.py
@@ -0,0 +1,32 @@
+print(1+1)
+print(3-2)
+print(5*2)
+print(6/3)
+
+print(2**3) # 제곱연산자
+print(5%3) # 나머지 연산자
+print(10%3)
+print(5//3) # 몫 연산자
+print(10//3)
+
+# 비교연산
+print(10>3) # True
+print(4>=7) # False
+print(10<3) # False
+print(5<=5) # True
+
+print(3 == 3) # 3은 3과 똑같다 ! -> True
+print(4 == 2) # False
+print(3+4 == 7) # True
+
+print(1 != 3) # 같지 않다 -> True
+print(not(1 != 3)) # False
+
+print((3>0) and (3<5)) # True
+print((3>0) & (3<5)) # True
+
+print((3>0) or (3>5)) # True
+print((3>0) | (3>5)) # True
+
+print(5>4>3) # True
+print(5>4>7) # False
---
+++ 6시간파이썬/operator3.py
@@ -0,0 +1,28 @@
+# 숫자 처리 함수
+
+# 절댓값
+print(abs(-5))
+
+# 제곱
+print(pow(4,2)) # 4의 2승 = 16
+
+# 최댓값
+print(max(5,12))
+
+# 최솟값
+print(min(5,12))
+
+# 반올림
+print(round(3.14))
+print(round(4.9))
+
+from math import *
+# 내림
+print(floor(4.66)) # 4
+
+# 올림
+print(ceil(3.14))
+
+# 제곱근
+print(sqrt(16)) # 4
+
